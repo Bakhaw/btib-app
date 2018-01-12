c3d187a260a5c953d92fe52d8d97ee3e8673e0e0
bakhaw <faissal.hattou@gmail.com>
2018-01-12 09:26:59 +0100

    app btib

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,80 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Field extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstInputValue: "",
-      secondInputValue: "",
-      textareaValue: "",
-      send: false,
-      sendError: false,
-      textareaError: false,
-    };
-  }
-
-  handleFirstInput = (e) => {
-    this.setState({
-      firstInputValue: e.target.value
-    })
-  }
-
-  handleSecondInput = (e) => {
-    this.setState({
-      secondInputValue: e.target.value
-    })
-  }
-
-  handleTextarea = (e) => {
-    this.setState({
-      textareaValue: e.target.value
-    })
-
-    this.state.textareaValue.length > 139
-    ? this.setState({ textareaError: true })
-    : this.setState({ textareaError: false })
-
-  }
-
-  sendForm = () => {
-    this.state.textareaError ? this.setState({ sendError: true }) : this.setState({ send: true })
-  }
-
   render() {
     return (
-      <div>
-      <input type="text"
-             placeholder={this.props.firstInput}
-             onChange={this.handleFirstInput}
-             value={this.state.firstInputValue}/>
-      <input type="text"
-             placeholder={this.props.secondInput}
-             onChange={this.handleSecondInput}
-             value={this.state.secondInputValue}/>
+      <form>
+        {this.props.multiline ? (
+          // si la props multiline={true}, affiche une textarea
+          <div>
+            <textarea
+              cols="30"
+              rows="10"
+              placeholder={this.props.name}
+              onChange={this.props.handleTextarea}
+            />
+            <div className="errorMessage">
+              <p>{this.props.textareaValue.length}/140</p>
+            </div>
+            {this.props.textareaValue.length > 140 && (
+              <p>La description doit contenir 140 caractères maximum</p>
+            )}
+          </div>
+        ) : (
+          // si la props multiline={false} ou non définie, affiche un input
+          <div>
+            <input
+              type="text"
+              placeholder={this.props.name}
+              value={this.props.inputValue}
+              onChange={this.props.handleInput}
+              onBlur={this.props.onBlur}
+            />
 
-        {this.state.textareaError &&
-          <p>Le message doit contenir 140 caractères max</p>
-        }
-        <textarea name="description"
-                  id="textarea"
-                  cols="30"
-                  rows="10"
-                  placeholder={this.props.textarea}
-                  onChange={this.handleTextarea}>
-        </textarea>
-        <h6>{!this.state.err ? `{${140 - this.state.textareaValue.length} }` : null }</h6>
-        <button type="submit"
-                  onClick={this.sendForm}>
-            {this.props.buttonName}
-          </button>
-          {this.state.send &&
-            <h5>Votre message à bien été envoyé {this.state.secondInputValue} !</h5>
-          }
-          {this.state.sendError &&
-            <h5 style={{ color: 'red' }}>Il y a une erreur dans le formulaire</h5>
-          }
-      </div>
-    )
+            {/* Messages d'erreur */}
+            {this.props.nameError && (
+              <p>{this.props.errorMessage}</p>
+            )}
+            {this.props.lastnameError && (
+              <p>{this.props.errorMessage}</p>
+            )}
+          </div>
+        )}
+      </form>
+    );
   }
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,160 @@
-import React, { Component } from 'react';
-import Field from './Field';
+import React, { Component } from "react";
+import Field from "./Field";
 
-import '../stylesheets/Form.css';
+import "../stylesheets/Form.css";
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lastname: "",
+      lastnameError: false,
+      name: "",
+      nameError: false,
+      textareaValue: "",
+      formError: false,
+      canSubmit: false
+    };
+  }
 
-  render() { 
+  handleName = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+
+  handleLastName = e => {
+    this.setState({
+      lastname: e.target.value
+    });
+  };
+
+  handleTextarea = e => {
+    this.setState({
+      textareaValue: e.target.value
+    });
+  };
+
+  verifierName = () => {
+
+    // si le prénom ne commence pas par une majuscule
+    if (
+      this.state.name.slice(0, 1) !== this.state.name.toUpperCase().slice(0, 1)
+    ) {
+      this.setState({
+        nameError: true,
+        formError: true
+      });
+    }
+
+    // reset des states pour ne pas garder l'erreur affichée en cas de correction
+    if (
+      this.state.nameError &&
+      this.state.name.slice(0, 1) === this.state.name.toUpperCase().slice(0, 1)
+    ) {
+      this.setState({
+        nameError: false,
+      });
+    }
+  };
+
+  verifierLastName = () => {
+    
+    // si le nom ne commence pas par une majuscule
+    if (
+      this.state.lastname.slice(0, 1) !==
+      this.state.lastname.toUpperCase().slice(0, 1)
+    ) {
+      this.setState({
+        lastnameError: true,
+        formError: true
+      });
+    }
+
+    // reset des states pour ne pas garder l'erreur affichée en cas de correction
+    if (
+      this.state.lastnameError &&
+      this.state.lastname.slice(0, 1) ===
+        this.state.lastname.toUpperCase().slice(0, 1)
+    ) {
+      this.setState({
+        lastnameError: false,
+      });
+    }
+  };
+
+  verifierForm = () => {
+    // reset du state si les erreurs on été corrigées
+    if (
+      this.state.formError &&
+      !this.state.lastnameError &&
+      !this.state.nameError
+    ) {
+      this.setState({
+        formError: false
+      });
+    }
+  }
+
+  onSubmit = () => {
+
+    // verification des states pour récuperer les erreurs
+    this.verifierName()
+    this.verifierLastName()
+
+    this.verifierForm()
+
+    // si il n'y à pas d'erreur dans le form, on peux l'envoyer
+    if (!this.state.formError) {
+      this.setState({
+        canSubmit: true
+      });
+    }
+  };
+
+  render() {
     return (
       <div className="form">
-        <h3>Informations:</h3>
-        <Field firstInput="Nom"
-               secondInput="Prénom"
-               textarea="Message"
-               buttonName="Envoyer"/>
+        {this.state.canSubmit ? (
+          // Si il n'y a pas d'erreur dans le form
+          <h3>Votre description à bien été envoyée {this.state.name}!</h3>
+        ) : (
+          // Si il y a encore des erreurs dans le form
+          <div>
+            <h3>Informations:</h3>
+            <Field
+              name="Prénom"
+              handleInput={this.handleName}
+              inputValue={this.state.name}
+              nameError={this.state.nameError}
+              errorMessage="Le Prénom doit commencer par une majuscule"
+              onBlur={this.verifierName}
+            />
+
+            <Field
+              name="Nom"
+              handleInput={this.handleLastName}
+              inputValue={this.state.lastname}
+              lastnameError={this.state.lastnameError}
+              errorMessage="Le Nom doit commencer par une majuscule"
+              onBlur={this.verifierLastName}
+            />
+
+            <Field
+              name="Description"
+              multiline={true}
+              handleTextarea={this.handleTextarea}
+              textareaValue={this.state.textareaValue}
+            />
+
+            <button type="button" onClick={this.onSubmit}>
+              Valider
+            </button>
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
- 
+
 export default Form;
